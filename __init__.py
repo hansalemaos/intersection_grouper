12b95c79362493d159ea75d71276986e6048d1d3
Hans Alemão <aulasparticularesdealemaosp@gmail.com>
2022-12-31 06:03:42 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,27 +2,27 @@ from flatten_everything import flatten_everything
 
 
 def group_lists_with_intersections(list_, keep_duplicates=False):
-    results = [tuple(x) for x in list_]
-    results2 = results.copy()
+    results = tuple((frozenset(x) for x in list_))
+    results2 = results
     for r in results:
-        rset = set(r)
         newresults2 = []
         for r2 in results2:
-            inter = tuple(rset.intersection(set(r2)))
-            if inter:
-                appe = r + r2
-                appe = tuple(set(appe))
-                newresults2.append(tuple(sorted(appe)))
+            if not isinstance(r2, (frozenset,set)):
+                sr=frozenset(r2)
             else:
-                newresults2.append(tuple(sorted(r2)))
-
-        results2 = newresults2.copy()
-        results2 = [x for x in results2 if x]
-        results2 = list(set(results2))
+                sr=r2
+            inter = (r.intersection(sr))
+            if inter:
+                appe = r.union(r2)
+                newresults2.append(((appe)))
+            else:
+                newresults2.append(sr)
+        results2 = set(((frozenset(x) for x in newresults2 if x)))
+    results2 = tuple(tuple(x) for x in results2)
     if keep_duplicates:
-        flattened = tuple(flatten_everything(results))
+        flattened = tuple(flatten_everything(list_))
         results2 = [
-            tuple(flatten_everything([[y] * flattened.count(y) for y in x]))
+            tuple(flatten_everything([(y,) * flattened.count(y) for y in x]))
             for x in results2
         ]
     return results2
